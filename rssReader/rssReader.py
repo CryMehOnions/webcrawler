0d5de5aacb5788df1e2e0659b82624c1ae8ced64
Miko Garcia <crymehonions@gmail.com>
2017-03-11 05:04:06 +0800

    again

--- rssReader/rssReader.py
+++ rssReader/rssReader.py
@@ -18,19 +18,22 @@ for data in dataDict:
 	title.remove(bound)
 	area = "-".join(title)
 
+	print area
+
 	entry = {'location_road' : road, 'location_area' : area, 'location_bound' : bound, 'traffic' : data.description, 'guid' : int(guid), 'timestamp' : data.published}
 	entryList.append(entry)
 
-try:
-	conn = psycopg2.connect("dbname='mmda_traffic' user='direksyon' host='localhost' password='gothere4lyf'")
-	print "Connection Successful"
-except:
-	print "Cannot connect to database"
 
-insertQuery = """INSERT INTO entries (location_road, location_area, location_bound, traffic, guid, timestamp) VALUES (%(location_road)s, %(location_area)s, %(location_bound)s, %(traffic)s, %(guid)s, %(timestamp)s) ON CONFLICT(guid) DO NOTHING"""
+# try:
+# 	conn = psycopg2.connect("dbname='mmda_traffic' user='direksyon' host='localhost' password='gothere4lyf'")
+# 	print "Connection Successful"
+# except:
+# 	print "Cannot connect to database"
 
-cur = conn.cursor()
-cur.executemany(insertQuery, tuple(entryList))
+# insertQuery = """INSERT INTO entries (location_road, location_area, location_bound, traffic, guid, timestamp) VALUES (%(location_road)s, %(location_area)s, %(location_bound)s, %(traffic)s, %(guid)s, %(timestamp)s) ON CONFLICT(guid) DO NOTHING"""
 
-conn.commit()
-conn.close()	
+# cur = conn.cursor()
+# cur.executemany(insertQuery, tuple(entryList))
+
+# conn.commit()
+# conn.close()	
