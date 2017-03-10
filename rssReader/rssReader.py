3944a503ace4774d24c9f7d9a811aeb9b2c22951
Miko Garcia <crymehonions@gmail.com>
2017-03-11 03:06:21 +0800

    mo' changes

--- rssReader/rssReader.py
+++ rssReader/rssReader.py
@@ -2,5 +2,8 @@ import psycopg2
 
 try:
 	conn = psycopg2.connect("dbname='mmda_traffic' user='direksyon' host='localhost' password='gothere4lyf'")
+	print "Connection Successful"
 except:
 	print "Cannot connect to database"
+
+cur = conn.cursor()
