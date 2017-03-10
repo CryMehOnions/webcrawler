4ac10ecebae7c200bec032432165fda4589d85fb
Miko Garcia <crymehonions@gmail.com>
2017-03-11 03:58:11 +0800

    Upgraded insert into upsert

--- rssReader/rssReader.py
+++ rssReader/rssReader.py
@@ -16,7 +16,7 @@ try:
 	conn = psycopg2.connect("dbname='mmda_traffic' user='direksyon' host='localhost' password='gothere4lyf'")
 	print "Connection Successful"
 
-	insertQuery = """INSERT INTO entries (location_road, location_area, location_bound, traffic, guid, timestamp) VALUES (%(location_road)s, %(location_area)s, %(location_bound)s, %(traffic)s, %(guid)i, %(timestamp)s)"""
+	insertQuery = """INSERT INTO entries (location_road, location_area, location_bound, traffic, guid, timestamp) VALUES (%(location_road)s, %(location_area)s, %(location_bound)s, %(traffic)s, %(guid)i, %(timestamp)s) ON CONFLICT(guid) DO NOTHING"""
 
 	cur = conn.cursor()
 
